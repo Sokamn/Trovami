1ba27daad7fd52a14a311d7d9d473372b0e612a4
Matias Alzú <matias.alzu143@gmail.com>
2024-01-14 02:26:17 -0300

    navigation implementation 14/01 02:26

--- app/src/main/java/com/sokamn/trovami/ui/MainActivity.kt
+++ app/src/main/java/com/sokamn/trovami/ui/MainActivity.kt
@@ -4,7 +4,15 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sokamn.trovami.R
 import com.sokamn.trovami.core.ex.toast
 import com.sokamn.trovami.data.source.datastore.DataStoreConstants.USER_KEY_PREFS
@@ -19,14 +27,99 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val test = findViewById<Button>(R.id.testbutton)
-        test.setOnClickListener {
-            toast("uid: " + intent.getStringExtra(USER_KEY_PREFS).toString())
+        initUI()
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    private fun initUI() {
+        setUIComponents()
+        initListeners()
+    }
+
+    private fun setUIComponents() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcvContainerAM) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    private fun initListeners() {
+        findViewById<BottomNavigationView>(R.id.bottomNav).apply {
+            homeSelected(this)
+            navController.let { navController ->
+                NavigationUI.setupWithNavController(
+                    this,
+                    navController
+                )
+                navController.addOnDestinationChangedListener(){ _, dest, _ ->
+                    when(dest.id){
+                        R.id.homeFragment-> homeSelected(this)
+                        //R.id.searchFragment-> homeSelected(this)
+                        R.id.collectionFragment-> savedSelected(this)
+                        //R.id.masterSavedFragment-> savedSelected(this)
+                        R.id.messageFragment-> messageSelected(this)
+                        R.id.profileFragment-> profileSelected(this)
+                        //R.id.makeJobFragment-> makeJobSelected(this)
+                    }
+                    /*aux = if(dest.id == R.id.masterSavedFragment){
+                        1
+                    }else{
+                        0
+                    }*/
+                }
+                setOnItemReselectedListener {
+                    val selectedMenuItemNavGraph =
+                        navController.graph.findNode(it.itemId) as NavGraph
+                    selectedMenuItemNavGraph.let { menuGraph ->
+                        /*if(aux == 1){
+                            findNavController(R.id.fcvContainerAM).navigate(MasterSavedFragmentDirections.actionMasterSavedFragmentToSavedFragment())
+                        }*/
+                        navController.popBackStack(menuGraph.startDestinationId, false)
+                    }
+                }
+            }
         }
+    }
+
+    private fun profileSelected(bottomNav: BottomNavigationView) {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = getColor(R.color.bg)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+
+    private fun messageSelected(bottomNav: BottomNavigationView) {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = getColor(R.color.tertiaryColor)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+    }
+
+    private fun savedSelected(bottomNav: BottomNavigationView) {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = getColor(R.color.bg)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+
+
+    private fun homeSelected(bottomNav: BottomNavigationView) {
+        window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = getColor(R.color.bg)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+
+    private fun makeJobSelected(bottomNav: BottomNavigationView) {
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
     }
 }
